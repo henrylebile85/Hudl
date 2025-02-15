c1c7fb145b97e26113afe0b10447edeec000ad6c
Henry Lebile <henry_lebile@yahoo.com>
2025-02-15 10:01:42 +0000

    yml

--- hudl/conftest.py
+++ hudl/conftest.py
@@ -44,12 +44,14 @@ def init_driver(request):
     if browser in ('chrome', 'ch', 'headlesschrome'):
         chrome_options = ChromeOptions()
         if browser == 'headlesschrome':
-            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--headless=new')  # New Chrome headless mode
+            chrome_options.add_argument('--disable-gpu')
             chrome_options.add_argument('--no-sandbox')
-            chrome_options.add_argument('--disable-gpu')  # Ensures smooth headless operation
+            chrome_options.add_argument('--disable-dev-shm-usage')  # Prevents shared memory issues
+            chrome_options.add_argument('--remote-debugging-port=9222')  # Helps debugging
+            chrome_options.add_argument(f'--user-data-dir=/tmp/chrome-user-data-{os.getpid()}')  # Fixes session issue
+            chrome_options.add_argument('--guest')  # Runs as a guest user
         # Set up the ChromeDriver using webdriver_manager
-            chrome_options.add_argument('--disable-dev-shm-usage')
-            chrome_options.add_argument('--remote-debugging-port=9222')
         service = Service(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=chrome_options)
         # you can also use options to write it this way to set up ChromeDriver (below)
